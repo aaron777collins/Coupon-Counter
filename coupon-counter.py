b1dc81223077919463109791769d6a53965e7206
Aaron Collins <aaron777collins@github.com>
2021-04-29 22:29:05 -0400

    Added custom codes and fixed key detection (plus added the ability to backspace while typing a code)

--- coupon-counter.py
+++ coupon-counter.py
@@ -13,6 +13,9 @@ dependencies.installAll()
 running = True
 readingChars = False
 cmdBuffer = ""
+codes = []
+codesName = "codes.txt"
+
 
 
 from pynput.keyboard import Key, Listener, Controller
@@ -48,22 +51,30 @@ def addToBuffer(key):
 	else:
 		cmdBuffer = "" + str(key.char)
 
+def printBuffer():
+	global cmdBuffer
+	print("Current input: " + cmdBuffer)
+
+
 def attemptExecution():
 	global cmdBuffer
 
-	print("Current input: " + cmdBuffer)
+	printBuffer()
 
-	if cmdBuffer == "m10":
-		clearBuffer()
-		changeMode() #stop reading input
+
+	for code in codes:
+		if cmdBuffer == code:
+			clearBuffer()
+			changeMode() #stop reading input
+			break
 
 def on_press(key):
-	pass
+	global running, readingChars, cmdBuffer
 
-def on_release(key):
-
-	global running, readingChars
-
+	if(key == Key.backspace):
+		if (cmdBuffer != None and len(cmdBuffer)!=0):
+			cmdBuffer = cmdBuffer[0:-1]
+			printBuffer()
 	if(key == Key.esc):
 		running=False
 		return False
@@ -77,15 +88,51 @@ def on_release(key):
 				addToBuffer(key)
 				attemptExecution()
 
+
 	except AttributeError:
 		pass
+
+
+
+
+
+def on_release(key):
+	pass
 
 listener = Listener( on_press=on_press, on_release=on_release)
 
 listener.start()
 
 
+def printCodes():
+	global codes
+
+	print("The following codes were detected:")
+	for code in codes:
+		print(code)
+
+
+def init():
+	global codes, codesName
+
+	if(not os.path.exists(codesName)):
+		with open(codesName, 'w') as reader:
+			reader.write("samplecode1, samplecode2")
+
+	with open(codesName, 'r') as reader:
+		fileData = reader.read()
+		fileList = fileData.split(",")
+		for item in fileList:
+			item = item.strip()
+			codes.append(item)
+
+
+	printCodes()
+
 def main():
+
+	init()
+
 	while(running):
 		time.sleep(1)
 	exit()
